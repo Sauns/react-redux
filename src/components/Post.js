21e58a28c1d8938696537944029996aea9fedb60
unknown <ustinovmmk@gmail.com>
2020-11-22 22:39:47 +0200

    push app

---
+++ src/components/Post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default ({ post }) => {
+	return (
+		<div className="card">
+			<div className="card-body">
+				<h1 className="card-title">{post.title}</h1>
+			</div>
+		</div> 
+	)
+}
---
+++ src/redux/actions.js
@@ -0,0 +1,59 @@
+import { CREATE_POST, FETCH_POST, HIDE_ALERT, HIDE_LOADER, SHOW_ALERT, SHOW_LOADER } from "./types";
+
+export function createPost(post) {
+	return {
+		type: CREATE_POST,
+		payload: post
+	}
+}
+
+export function showLoader() {
+	return {
+		type: SHOW_LOADER
+	}
+}
+
+export function hideLoader() {
+	return {
+		type: HIDE_LOADER
+	}
+}
+
+export function showAlert(text, time = 2000) {
+	return dispatch => {
+		dispatch({
+			type: SHOW_ALERT,
+			payload: text
+		})
+
+		setTimeout(() => {
+			dispatch(hideAlert)
+		}, time)
+	}
+}
+
+export function hideAlert() {
+	return {
+		type: HIDE_ALERT
+	}
+}
+
+export function fetchPosts() {
+	return async dispatch => {
+		try {
+			dispatch(showLoader())
+			const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+			const json = await response.json()
+	
+			dispatch({
+				type: FETCH_POST,
+				payload: json
+			})
+			dispatch(hideLoader())
+		} catch(e){
+			dispatch(showAlert('Some server error'))
+			dispatch(hideLoader())
+		}
+		
+	}
+}
